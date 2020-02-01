159213121462402ec0a9646829fccdc71ff071ca
gordon13 <paullehenaff@msn.com>
2020-02-01 23:55:21 +0000

    - Added music manager

---
+++ SupermarketSmash/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    GameMan gameManager;
+    
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameMan>();
+        AkSoundEngine.SetState("Happy_Hectic", "Happy_Hectic");
+        AkSoundEngine.SetRTPCValue("Music_RTPC", 0);
+        AkSoundEngine.PostEvent("Game_Music_Event", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AkSoundEngine.SetRTPCValue("Music_RTPC", gameManager.itemsOffShelves);
+    }
+}
