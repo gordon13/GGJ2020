a245fdaae899f929c995d38a6268ca4def7cee43
gordon13 <paullehenaff@msn.com>
2020-02-01 09:11:52 +0000

    Initial commit

---
+++ SupermarketSmash/Assets/Scripts/Baby.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baby : MonoBehaviour
+{
+    private float _timer = 0f;
+    private Rigidbody _rigidbody;
+    public float force = 100f;
+
+    void Start()
+    {
+        _rigidbody = GetComponentInChildren<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Launch(force);
+        }
+    }
+
+    void Launch(float force)
+    {
+        Debug.Log("Launch!");
+        _rigidbody.AddForce(new Vector3(0, force, 0));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Launch(force);
+    }
+}
---
+++ SupermarketSmash/Assets/Scripts/Shelf.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+    public GameObject itemPrefab;
+
+    private ShelfItemPlacement[] itemPlaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemPlaces = GetComponentsInChildren<ShelfItemPlacement>();
+        foreach (ShelfItemPlacement p in itemPlaces)
+        {
+            GameObject item = Instantiate(itemPrefab, p.transform.position, p.transform.rotation);
+        }
+        Time.timeScale = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (ShelfItemPlacement p in itemPlaces)
+        {
+            
+        }
+    }
+}
