aeac70b752b8b6ee43f656da361c205a21fc48c8
gordon13 <paullehenaff@msn.com>
2020-02-01 17:48:33 +0000

    - Added impact sound script

---
+++ SupermarketSmash/Assets/Scripts/OnImpactSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnImpactSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip impact;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Bone"))
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = impact;
+            audioSource.Play();
+        }
+    }
+}
