64a954010b0da1db891d4b8c99a753264bdb8d20
gordon13 <paullehenaff@msn.com>
2020-02-02 12:20:05 +0000

    Added loading video

--- SupermarketSmash/Assets/StreamVideo.cs
+++ SupermarketSmash/Assets/StreamVideo.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class StreamVideo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+
     void Start()
     {
-        
+        StartCoroutine(PlayerVideo());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PlayerVideo()
     {
-        
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+
     }
 }
